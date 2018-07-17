94b9829d3bdc2c2c6888651bff5d9a8b25f84f1a
Kristen Shea <kstar61@live.com>
2018-07-17 18:16:18 -0500

    Add project files.

---
+++ CG 2-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CG_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Length of rectangle: ");
+            String rectangleLength;
+            rectangleLength = Console.ReadLine();
+            int rectangleLengthInt = int.Parse(rectangleLength);
+            
+
+            Console.Write("Width of rectangle: ");
+            String rectangleWidth;
+            rectangleWidth = Console.ReadLine();
+            int rectangleWidthInt = int.Parse(rectangleWidth);
+
+            Console.WriteLine("Rectangle Area: " + " " + (rectangleLengthInt * rectangleWidthInt) );
+            Console.ReadLine();
+
+
+        }
+    }
+}
